802b3ca5dc978bafac137255500a8d655ca6b5d2
ExtremeGrief <59370607+ExtremeGrief@users.noreply.github.com>
2020-10-30 16:07:53 +0200

    Update .BAT file to comply with possible CloudForce partnership.
    Update .BAT file to comply with possible CloudForce partnership.
    
    Signed: ExtremeGrief

--- ssfn_gccs.gui.bat
+++ ssfn_gccs.gui.bat
@@ -1,36 +1,31 @@
-@echo off
-    title GCS GUI , Port007 - All Rights Reserved
-    %HOMEDRIVE%
-    CD "%PROGRAMFILES(X86)%\Steam"
+@ECHO OFF
+TITLE GCS GUI , Port007 - All Rights Reserved
+%HOMEDRIVE%
+CD "%PROGRAMFILES(X86)%\Steam"
 :FOUND
 %HOMEDRIVE%
-cls
-echo Ctrl + V in Console!, Using depot from USE Project by SoftwareRAT
-:wgetcheck:
-cd %PROGRAMFILES(X86)%\Steam"
+CLS
+ECHO Thanks to ExtremeGrief for workshop depot.
+CD %PROGRAMFILES(X86)%\Steam"
 IF EXIST "%PROGRAMFILES(X86)%\Steam\steamapps\content\" (ren "%PROGRAMFILES(X86)%\Steam\steamapps\content\" "OGcontent") >NUL 2>&1
-mklink /J "%PROGRAMFILES(X86)%\Steam\steamapps\content\" "C:\Users\kiosk\AppData\Local\Temp\"
-start "" "steam://open/console"
-echo download_item 304930 2221493609 | clip
-cls
-echo Waiting for WGET for Windows...
-timeout /T 3 >NUL
+MKLINK /J "%PROGRAMFILES(X86)%\Steam\steamapps\content\" "C:\Users\kiosk\AppData\Local\Temp\"
+START "" steam.exe "steam://open/console"
+ECHO download_item 304930 2236589697|clip
+ECHO Ctrl + V in Console.
+TIMEOUT /T 3 /NOBREAK >NUL
 :CHECKLOOP
-IF EXIST "C:\Users\kiosk\AppData\Local\Temp\app_304930\item_2221493609\files\HERE\wget.exe" GOTO FOUND >NUL 2>&1
-GOTO NOTFOUND
-:NOTFOUND
+IF EXIST "C:\Users\kiosk\AppData\Local\Temp\app_304930\item_2236589697\bruh\downloader.exe" GOTO FOUND >NUL 2>&1
 GOTO CHECKLOOP
 :FOUND
-cls
-echo Downloading GCCS...
-%HOMEDRIVE%
-cd \
-md GCCStemp
-cd "%PROGRAMFILES(X86)%\Steam\"
-timeout /T 5 >NUL
-"C:\Users\kiosk\AppData\Local\Temp\app_304930\item_2221493609\files\HERE\wget.exe" -O "C:\Users\kiosk\AppData\Local\Temp\GCCS.exe" -q "https://github.com/devporter007/gcs.utility/releases/download/0.0.00001/gcsgui.exe"
-timeout /t 3 /NOBREAK >NUL 2>&1
-cls
-start "" "C:\Users\kiosk\AppData\Local\Temp\GCCS.exe"
-exit
+CLS
+ECHO Downloading GCS...
+B:
+MD "B:\GCS
+TIMEOUT /T 5 >NUL
+SET DL="C:\Users\kiosk\AppData\Local\Temp\app_304930\item_2236589697\bruh\downloader.exe"
+%DL% -LJOk "https://github.com/devporter007/gcs.utility/releases/download/0.0.00001/gcsgui.exe"
+TIMEOUT /t 3 /NOBREAK >NUL 2>&1
+CLS
+START "" "B:\GCS\GCCS.exe"
+EXIT
 

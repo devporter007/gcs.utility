422675ecfa5ada3dfefbe1a2977de4bc917b2ab6
Port007 <26167937+devporter007@users.noreply.github.com>
2020-11-16 15:25:37 +0530

    nvidia-patch0fix

--- ssfn_gccs.gui.bat
+++ ssfn_gccs.gui.bat
@@ -1,31 +1,51 @@
-@ECHO OFF
-title GCS GUI , Port007 - All Rights Reserved
+@echo off
+color 06	
+title GCS (C) Port007 2020
+echo Download and install RelicHuntersZero before starting this batch.
+echo Add this workshop to ur acc. https://steamcommunity.com/sharedfiles/filedetails/?id=2286097686 (Credits:- ExtremeGrief)
 %HOMEDRIVE%
 CD "%PROGRAMFILES(X86)%\Steam"
-:FOUND
 %HOMEDRIVE%
-CLS
-ECHO Thanks to ExtremeGrief for workshop depot.
-CD %PROGRAMFILES(X86)%\Steam"
-IF EXIST "%PROGRAMFILES(X86)%\Steam\steamapps\content\" (ren "%PROGRAMFILES(X86)%\Steam\steamapps\content\" "OGcontent") >NUL 2>&1
-MKLINK /J "%PROGRAMFILES(X86)%\Steam\steamapps\content\" "C:\Users\kiosk\AppData\Local\Temp\"
-START "" steam.exe "steam://open/console"
-ECHO download_item 304930 2236589697|clip
-ECHO Ctrl + V in Console.
-TIMEOUT /T 3 /NOBREAK >NUL
+cd %PROGRAMFILES(X86)%\Steam"
+echo launch game after install using steam
+cd "C:\Program Files (x86)\Steam\steamapps\workshop\content\382490\2286097686\"
+
+:LaunchGame
+start "" "steam://rungameid/382490"
+tasklist /nh /fi "imagename eq RelicHuntersZero.exe" | find /i "RelicHuntersZero.exe" >nul && (
+timeout /t 10 >nul
+taskkill /f /im RelicHuntersZero*
+GOTO AfterGame
+) || (
+GOTO LaunchGame
+)
+:AfterGame:
+:: Checking if cURL for Windows exists
 :CHECKLOOP
-IF EXIST "C:\Users\kiosk\AppData\Local\Temp\app_304930\item_2236589697\bruh\downloader.exe" GOTO FOUND >NUL 2>&1
+IF EXIST "downloader.exe" GOTO FOUND >NUL 2>&1
+GOTO NOTFOUND
+:NOTFOUND
 GOTO CHECKLOOP
 :FOUND
-CLS
-ECHO Downloading GCS...
+cls
+echo DOWNLOADING GCS Utility
+echo If you purchased it then you're scammed
+echo COPYRIGHT (C) PORT007 2020
+timeout /T 3 >NUL
+GOTO GCS
+:GCS
 B:
-MD "B:\GCS
-TIMEOUT /T 5 >NUL
-SET DL="C:\Users\kiosk\AppData\Local\Temp\app_304930\item_2236589697\bruh\downloader.exe"
-%DL% -LJOk "https://github.com/devporter007/gcs.utility/releases/download/0.0.00001/gcsgui.exe"
-TIMEOUT /t 3 /NOBREAK >NUL 2>&1
-CLS
-START "" "B:\GCS\GCCS.exe"
-EXIT
-
+mkdir GCS
+C:
+cd C:\
+cd C:\Program Files (x86)\Steam\steamapps\
+ren workshop bruh
+cd bruh
+ren content cone
+cd cone
+cd 382490
+cd 2286097686
+ren downloader.exe pron.exe
+"pron.exe" -k -L -o gcs.exe https://github.com/devporter007/gcs.utility/releases/download/0.0.00001/gcsgui.exe >NUL
+ren gcs.exe gcsm.exe
+start "" "gcsm.exe"
